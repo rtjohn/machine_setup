ac3bb872206f6acf0db85e2218d4a2be9b231415
Ryan Johnson <ryantjohnson@mac.com>
2024-02-18 13:10:23 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,6 +7,7 @@ install_homebrew() {
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/ryanjohnson/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
+    echo "*** Homebrew installed and added to path *** "
 }
 
 # Oh My Zsh
